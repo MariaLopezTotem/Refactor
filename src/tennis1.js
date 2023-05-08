d86798d9fed16678c7ba8f96912d154ed6c43c69
Maria <maria.lopez@totemtech.net>
2023-05-08 08:29:41 -0600

    respaldo

--- src/tennis1.js
+++ src/tennis1.js
@@ -1,33 +1,47 @@
 "use strict";
+const _ = require('lodash');
+
+const SCORE = {
+  loveAll : 'Love-All',
+  fifteenAll : 'Fifteen-All',
+  thirtyAll : 'Thirty-All',
+  deuce : 'Deuce',
+
+  advantagePlayer1 : 'Advantage player1',
+  advantagePlayer2 : 'Advantage player2',
+  winForPlayer1 : 'Win for player1',
+  winForPlayer2 : 'Win for player2'
+
+}
 
 function getScore(m_score1, m_score2) {
-  var score = "";
-  var tempScore = 0;
-  if (m_score1 === m_score2) {
+  let score = "";
+  let tempScore = 0;
+  if (_.isEqual(m_score1, m_score2)) {
     switch (m_score1) {
       case 0:
-        score = "Love-All";
+        score = SCORE.loveAll;
         break;
       case 1:
-        score = "Fifteen-All";
+        score = SCORE.fifteenAll;
         break;
       case 2:
-        score = "Thirty-All";
+        score = SCORE.thirtyAll;
         break;
       default:
-        score = "Deuce";
+        score = SCORE.deuce;
         break;
     }
   } else if (m_score1 >= 4 || m_score2 >= 4) {
-    var minusResult = m_score1 - m_score2;
+    let minusResult = m_score1 - m_score2;
     if (minusResult === 1) {
-      score = "Advantage player1";
+      score = SCORE.advantagePlayer1;
     } else if (minusResult === -1) {
-      score = "Advantage player2";
+      score = SCORE.advantagePlayer2;
     } else if (minusResult >= 2) {
-      score = "Win for player1";
+      score = SCORE.winForPlayer1;
     } else {
-      score = "Win for player2";
+      score = SCORE.winForPlayer2;
     }
   } else {
     for (var i = 1; i < 3; i++) {
